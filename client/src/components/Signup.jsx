51b479d529bb553179f5aa2c39cd636f8d95e0ad
noelfoka <nomgnenoel@gmail.com>
2024-04-21 14:12:29 +0100

    create username label

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,10 @@
 const Signup = () => {
   return (
-    <div>
-      Sign up
+    <div className="sign-up-container">
+      <h2>Sign Up</h2>
+      <form className="sign-up-form">
+        <label htmlFor="username">Username</label>
+      </form>
     </div>
   )
 }
