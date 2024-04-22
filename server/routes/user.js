0d6dbc8b23b066fef0fbdb34158ad9ee8fd52aa3
noelfoka <nomgnenoel@gmail.com>
2024-04-22 11:17:51 +0100

    up

---
+++ server/routes/user.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
+})
