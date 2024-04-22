7d479465c7955ab1d27e75576120d38f18ce7684
noelfoka <nomgnenoel@gmail.com>
2024-04-22 07:24:11 +0100

    upd

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import "../App.css";
+
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+const [email, setEmail] = useState('');
 
 const Signup = () => {
   return (
@@ -6,13 +11,29 @@ const Signup = () => {
       <form className="sign-up-form">
         <h2>Sign Up</h2>
         <label htmlFor="username">Username: </label>
-        <input type="text" id="username" placeholder="Username" />
+        <input 
+          type="text"  
+          id="username" 
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+       />
 
         <label htmlFor="email">Email: </label>
-        <input type="text" id="email" autoComplete="off" placeholder="Email" />
+        <input 
+          type="text" id="email" 
+          autoComplete="off" 
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="password">Password: </label>
-        <input type="password" id="password" autoComplete="off" placeholder="********" />
+        <input 
+          type="password" 
+          id="password" 
+          autoComplete="off" 
+          placeholder="********"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button type="submit">Sign Up</button>
       </form>
