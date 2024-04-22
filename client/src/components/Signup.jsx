51e552dce699ea57f5d2d680d064e50da29a617b
noelfoka <nomgnenoel@gmail.com>
2024-04-22 06:37:10 +0100

    update

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -3,7 +3,14 @@ const Signup = () => {
     <div className="sign-up-container">
       <h2>Sign Up</h2>
       <form className="sign-up-form">
-        <label htmlFor="username">Username</label>
+        <label htmlFor="username">Username: </label>
+        <input type="text" id="username" placeholder="Username" />
+
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email" autoComplete="off" placeholder="Email" />
+
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" autoComplete="off" placeholder="********" />
       </form>
     </div>
   )
