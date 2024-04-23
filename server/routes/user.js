bf3fd0e61cef6f468b0df8fd0f253541563031f3
noelfoka <nomgnenoel@gmail.com>
2024-04-23 02:12:43 +0100

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,7 +10,14 @@ router.post('/signup', async (req, res) => {
   if (user) {
     return res.json({ message: 'User already exists' });
   }
-  const hashpassword = await bcrypt.hash(password, 10 )
+  const hashpassword = await bcrypt.hash(password, 10 );
+  const newUser = new User({
+    email,
+    password: hashpassword,
+    password
+  });
+  await newUser.save();
+  return res.json({ message: 'User created successfully' });
 })
 
 export { router as UserRouter };
