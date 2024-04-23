11da63d3209af502266ff70fae918a159af5edb7
noelfoka <nomgnenoel@gmail.com>
2024-04-23 02:08:33 +0100

    updat

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,9 +4,13 @@ import {  User } from '../models/user';
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   const { username, email, password } = req.body;
-  User.findOne({ email })
+  const user = User.findOne({ email })
+  if (user) {
+    return res.json({ message: 'User already exists' });
+  }
+  const hashpassword = await bcrypt.hash(password, 10 )
 })
 
 export { router as UserRouter };
