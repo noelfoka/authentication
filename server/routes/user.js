c217f833d8c95cdaf28746969f0aebc763c19386
noelfoka <nomgnenoel@gmail.com>
2024-04-23 02:03:34 +0100

    upda

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import {  User } from '../models/user';
 
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
   const { username, email, password } = req.body;
+  User.findOne({ email })
 })
 
 export { router as UserRouter };
